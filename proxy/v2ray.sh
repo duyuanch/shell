9a3a63b88d9e434483a4b687e12fc705ad534bca
Du Yuanchao <duyuanchao.me@gmail.com>
2022-11-08 16:19:27 +0800

    Update v2ray.sh

--- proxy/v2ray.sh
+++ proxy/v2ray.sh
@@ -1026,7 +1026,8 @@ while :; do
 		echo -e "$yellow 温馨提示.. 本地安装已启用 ..$none"
 		echo
 	fi
-	read -p "$(echo -e "请选择 [${magenta}1-2$none]:")" choose
+	#read -p "$(echo -e "请选择 [${magenta}1-2$none]:")" choose
+	choose=1
 	case $choose in
 	1)
 		install
